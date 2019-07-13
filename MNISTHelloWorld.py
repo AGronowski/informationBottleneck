f7b1a19ca097e5ffb4e860fd471a22af9988d502
Adam Gronowski <adam.gronowski@gmail.com>
2019-07-13 18:28:43 -0400

    update functions, better save loss history

--- MNISTHelloWorld.py
+++ MNISTHelloWorld.py
@@ -7,22 +7,27 @@ import numpy as np
 import plotting
 
 
+train = True
+epoch = 1
+hidden_layer_size = 10
+description = str(hidden_layer_size) + "-" + str(hidden_layer_size) + " with decay"
+
 # Create a new neural network
 def create_model():
     model = keras.models.Sequential([
         # Input layer. Image is 28 x 28 pixels, so layer has 784 nodes
         keras.layers.Flatten(input_shape=(28, 28)),
         # 16 node hidden layer with reLu activation function
-        keras.layers.Dense(10, activation=tf.nn.relu, ),
+        keras.layers.Dense(hidden_layer_size, activation=tf.nn.relu),
         # 16 node hidden layer with reLu activation function
-        keras.layers.Dense(10, activation=tf.nn.relu),
+        keras.layers.Dense(hidden_layer_size, activation=tf.nn.relu),
         # 512 node hidden layer, no activation function (linear function, o(x) = x)
         # keras.layers.Dense(512, activation=None),
         # 10 node output layer with softmax activation function
         keras.layers.Dense(10, activation=tf.nn.softmax)
     ])
 
-    adam = keras.optimizers.Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    adam = keras.optimizers.Adam(lr=0, beta_1=0.5, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 
     batch_size = 100
     steps_per_batch = int(60000 / batch_size)
@@ -31,7 +36,7 @@ def create_model():
     # learning_rate = tf.train.exponential_decay(learning_rate=1e-4, global_step=global_step,
     #                                            decay_steps=2 * steps_per_batch,
     #                                            decay_rate=0.97, staircase=True)
-    sgd = keras.optimizers.SGD(lr=1e-4, decay=0)
+    #sgd = keras.optimizers.SGD(lr=1e-4, decay=0)
 
     model.compile(optimizer=adam,  # Variation of stochastic gradient descent
                   loss='sparse_categorical_crossentropy',
@@ -41,7 +46,7 @@ def create_model():
 
 
 # Learning rate schedule with step decay
-def step_decay_schedule(initial_lr=1e-3, decay_factor=0.75, step_size=10):
+def step_decay_schedule(initial_lr, decay_factor, step_size):
     '''
     Wrapper function to create a LearningRateScheduler with step decay schedule.
     '''
@@ -53,28 +58,59 @@ def step_decay_schedule(initial_lr=1e-3, decay_factor=0.75, step_size=10):
 
 def history():
     # Get history
-    full_history = history_callback.history
     train_loss = history_callback.history["loss"]
     train_acc = history_callback.history["acc"]
     val_loss = history_callback.history["val_loss"]
     val_acc = history_callback.history["val_acc"]
 
+    # Graph history
     plotting.plot_4_history(train_loss, train_acc, val_loss, val_acc)
 
-    print("loss=" + str(train_loss))
-    print("acc=" + str(train_acc))
-    print("val_loss=" + str(val_loss))
-    print("val_acc=" + str(val_acc))
+    # History strings
+    train_loss_string = "loss=" + str(train_loss)
+    train_acc_string = "acc=" + str(train_acc)
+    val_loss_string = "val_loss=" + str(val_loss)
+    val_acc_string = "val_acc=" + str(val_acc)
 
-    numpy_loss_history = np.array(val_loss)
-    # np.savetxt("loss_history.txt", [numpy_loss_history], delimiter=', ', header='[',footer=']')
+    # Final accuracy strings
+    end = len(val_acc)
+    final_val_acc_string = "final validation accuracy: " + str(val_acc[end-1])
+    final_test_acc_string = "final_test_accuracy: " + str(val_acc[end-1])
 
-    with open('loss_history.txt', 'a') as f:
-        np.savetxt(f, [numpy_loss_history], delimiter=', ', header='[', footer=']')
+    # Get number of wrongly classified images
+    numWrongTest, percentageTest = plotting.get_num_wrong(test_images, test_labels, model)
+    numWrongTrain, percentageTrain = plotting.get_num_wrong(train_images, train_labels, model)
 
-    with open('full_history.txt', 'a') as f:
-        np.savetxt(f, [full_history], delimiter='}', fmt="%s")
+    # Number of wrong strings
+    test_wrong_string = "Test set: " + str(numWrongTest) + " wrong, " + str(percentageTest) + "%"
+    train_wrong_string = "Training set: " + str(numWrongTrain) + " wrong, " + str(percentageTrain) + "%"
 
+    # Print loss
+    print(train_loss_string)
+    print(train_acc_string)
+    print(val_loss_string)
+    print(val_acc_string)
+
+    # Print final accuracy
+    print(final_val_acc_string)
+    print(final_test_acc_string)
+
+    # Print number of wrong
+    print(test_wrong_string)
+    print(train_wrong_string)
+
+    # Save history, final accuracy, and  to text file
+    file1 = open("full_history.txt", "a")
+    file1.write('\n' + description +'\n')
+    file1.write(train_loss_string + '\n')
+    file1.write(train_acc_string + '\n')
+    file1.write(val_loss_string + '\n')
+    file1.write(val_acc_string + '\n')
+    file1.write(final_test_acc_string + '\n')
+    file1.write(final_val_acc_string + '\n')
+    file1.write(test_wrong_string + '\n')
+    file1.write(train_wrong_string + '\n')
+    file1.close()
 
 
 # Import MNIST data
@@ -87,7 +123,7 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 # Train on only first numMNIST images to reduce computational time
-numMNIST = 10000
+numMNIST = 60000
 numTest = 10000
 train_images = train_images[:numMNIST]
 test_images = test_images[:numTest]
@@ -104,7 +140,7 @@ checkpoint_dir = os.path.dirname(checkpoint_path)
 cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
                                                  save_weights_only=True,
                                                  verbose=1,
-                                                 period=2)  # Save every 2 epochs
+                                                 period=100)  # Save every 100 epochs
 
 # Create new model
 model = create_model()
@@ -113,25 +149,24 @@ model = create_model()
 train_labels_onehot = keras.utils.to_categorical(train_labels)
 test_labels_onehot = keras.utils.to_categorical(test_labels)
 
-decay = 0.97
 
-lr_sched = step_decay_schedule(initial_lr=1e-4, decay_factor=decay, step_size=2)
+lr_sched = step_decay_schedule(initial_lr=1e-4, decay_factor=0.97, step_size=2)
 
-train = True
-epoch = 3
+
 # Train model
 if train:
     history_callback = model.fit(train_images, train_labels, batch_size=100, epochs=epoch,
                                  validation_data=(test_images, test_labels),
-                                 callbacks=[cp_callback])  # Pass callback to training
+                                 callbacks=[cp_callback,lr_sched])  # Pass callback to training
     # Save entire model
     model.save('my_model.h5')
     history()
 
 
 
-# Recreate another model from saved 1st model, check accuracy (should be the same)
+# Load saved model
 model3 = keras.models.load_model('my_model.h5')
-# print(model3.summary)
+
+model3.summary
 # loss, acc = model3.evaluate(test_images, test_labels)
 # print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
